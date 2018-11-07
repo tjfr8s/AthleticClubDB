7366ce7941f23830e7502b9701f9dfb3f7967edd
Tyler Freitas <tjfr8s@gmail.com>
2018-11-07 10:23:32 -0800

    Add update bill function to membership display.

--- athletic_club_web_app/memberships.js
+++ athletic_club_web_app/memberships.js
@@ -13,15 +13,26 @@ module.exports = function(){
         });
     }
 
+    function updateBills(res, mysql, context, complete){
+        mysql.pool.query("UPDATE membership, (SELECT family.family_id, membership.membership_id, person.fname, person.person_id, person_service.service_id, sum(service.cost) AS sumCost FROM membership JOIN family ON family.membership_id = membership.membership_id JOIN person ON person.family_id = family.family_id JOIN person_service ON person.person_id = person_service.person_id JOIN service ON person_service.service_id = service.service_id GROUP BY family.family_id) t SET membership.bill = t.sumCost WHERE membership.membership_id = t.membership_id;", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            complete();
+        });
+    }
+
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
         context.jsscripts = [];
         var mysql = req.app.get('mysql');
+        updateBills(res, mysql, context, complete);
         getMemberships(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 2){
                 res.render('add_delete_memberships', context);
             }
         }
