5eb4c003a19d41eb3e4767b785cb324cb760b837
Tyler Freitas <tjfr8s@gmail.com>
2018-11-08 13:41:47 -0800

    Fixed error where bill wasn't updating on deleting a person caused by bad sql query. Fixed by performing left join on with person table while updating.

--- athletic_club_web_app/people.js
+++ athletic_club_web_app/people.js
@@ -1,6 +1,7 @@
 module.exports = function(){
     var express = require('express');
     var router = express.Router();
+    var updateBills = require('./public/update_bills.js');
 
     function getPeople(res, mysql, context, complete){
         mysql.pool.query("SELECT person_id, lname, fname, family_id, date_of_birth, zip_code, state, street_name FROM person", function(error,results, fields){
@@ -24,12 +25,10 @@ module.exports = function(){
         });
     }
 
-    router.delete('/:id', function(req, res){
+    router.delete('/:id', function(req, res, next){
         var mysql = req.app.get('mysql');
         var sql = "DELETE FROM person WHERE person_id = ?";
         var inserts = [req.params.id];
-        console.log(sql);
-        console.log(inserts);
         sql = mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
@@ -38,10 +37,24 @@ module.exports = function(){
                 console.log("400");
             }
             else{
-                res.status(202).end();
-                console.log("202");
+                next();
+                res.status(202);
             }
         });
+    }, function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = [];
+        var mysql = req.app.get('mysql');
+        updateBills.updateBills(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                console.log("delete");
+                res.end();
+            }
+        }
+
     });
 
     router.get('/', function(req, res){
